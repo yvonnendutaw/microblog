ff0e7bfc4e3435aa2206123d557de1533392cd36
yvonnendutaw <yvonita7@gmail.com>
2016-05-05 23:53:54 +0300

    changed views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 #!usr/bin/python
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Yvonne'}  # fake user
-    #renders the existing template ie the index.html template
-    return render_template('index.html'
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
                            title='Home',
                            user=user)
 
