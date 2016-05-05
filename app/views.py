ddfe2c9fbafadeced8c710794d3161662456bf45
yvonnendutaw <yvonita7@gmail.com>
2016-05-05 08:45:40 +0300

    template part 1

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Yvonne'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
