bbe814bb3ca2adcc1233a7c4e178b2b18038fc11
yvonnendutaw <yvonita7@gmail.com>
2016-05-07 23:21:26 +0300

    working form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +9,12 @@ def index():
     return render_template('index.html',
 #                           title='Home',
                            user=user)
+
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
