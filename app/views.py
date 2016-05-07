621097ce100e1ad4a0ccf626e412d46b9e7dbaf3
yvonnendutaw <yvonita7@gmail.com>
2016-05-07 23:31:10 +0300

    receiving and validation of data

--- app/views.py
+++ app/views.py
@@ -10,11 +10,15 @@ def index():
 #                           title='Home',
                            user=user)
 
-# index view function suppressed for brevity
+# login form
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
