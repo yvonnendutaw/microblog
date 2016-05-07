700b1641ca3d28435010b8f73efd501963df7590
yvonnendutaw <yvonita7@gmail.com>
2016-05-07 23:47:49 +0300

    added shebang

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+#!flask/bin/python
 from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#!flask/bin/python
 WTF_CSRF_ENABLED = True #requests cross-site reqest forgery
 SECRET_KEY = 'you-will-never-guess' #validates form
 
