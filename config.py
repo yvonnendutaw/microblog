453bb52ade62c310a9c1766baceb30e67c5c6dcb
yvonnendutaw <yvonita7@gmail.com>
2016-05-07 23:45:09 +0300

    added opeids to the form

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 WTF_CSRF_ENABLED = True #requests cross-site reqest forgery
 SECRET_KEY = 'you-will-never-guess' #validates form
+
+#openids
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
