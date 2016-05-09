e720c0ac981d8d9dfe3b3dd964556000e15a52dc
yvonnendutaw <yvonita7@gmail.com>
2016-05-09 10:28:20 +0300

    added routes

--- app/views.py
+++ app/views.py
@@ -5,8 +5,9 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    user = {'nickname': 'Yvonne nduta'}  # fake user
+    user = {'nickname': 'guest'}  # fake user
     return render_template('index.html',
 #                           title='Home',
                            user=user)
@@ -24,3 +25,13 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])#provides openid support
+
+@app.route('/')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/')
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
