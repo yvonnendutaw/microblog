35087a8608511e0ea9e903785f133c3655674845
yvonnendutaw <yvonita7@gmail.com>
2016-05-24 17:50:14 +0300

    image uploader

--- app/views.py
+++ app/views.py
@@ -1,44 +1,32 @@
-from flask import Flask, flash, redirect, render_template
+#!usr/bin/python
+#!flask/bin/python
+import os
+from flask import Flask, render_template, request
 from app import app
-from .forms import LoginForm
 
-guesses = ['Python', 'Java', 'C++']
+APP_ROOT = os.path.dirname(os.path.abspath((__file__))
 
-@app.route('/')
+@app.route("/")
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        },
-        { 
-            'author': {'nickname': 'Jane'}, 
-            'body': 'Hi i am Jane!' 
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    return render_template("upload.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+@app.route("/upload")
+def upload():
+    target = os.path.join(APP_ROOT, "images/")
+    print(target)
+    @app.route("/upload")
+    if not os.path.isdirname(target):
+        os.mkdir(target)
+    for file in request.files.getlist("file")
+        print file
+        filename = file.filename
+        destination = "/".join([target,filename])
+        print destination
+        file.save(destination)
+    return render_template("complete.html")
 
-@app.route('/guess/<int:id>')
-def guess(id):
-    return render_template('guess.html', 
-                           guess=guesses[id])
+if __name__ ==" __main__":
+app.run(debug=True)
+
+
+
